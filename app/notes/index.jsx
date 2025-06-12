a2f875a01b15d5c15d6c5b672b5bdc81e2ec37ae
Liudmila Rodzina <liudmilarodzina@gmail.com>
2025-06-12 19:29:46 +0300

    init: initialize app with UI components and styling

---
+++ app/notes/index.jsx
@@ -0,0 +1,92 @@
+import AddNoteModal from '@/components/AddNoteModal';
+import NoteList from '@/components/NoteList';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+const NoteScreen = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: '1',
+      text: 'Note One',
+    },
+    {
+      id: '2',
+      text: 'Note Two',
+    },
+    {
+      id: '3',
+      text: 'Note Three',
+    },
+    {
+      id: '4',
+      text: 'Note Four',
+    },
+    {
+      id: '5',
+      text: 'Note Five',
+    },
+  ]);
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [newNote, setNewNote] = useState('');
+
+  const addNote = () => {
+    if (newNote.trim() === '') return;
+
+    setNotes((prevNotes) => [
+      ...prevNotes,
+      {
+        id: Date.now.toString(),
+        text: newNote,
+      },
+    ]);
+
+    setNewNote('');
+    setModalVisible(false);
+  };
+
+  return (
+    <View style={styles.container}>
+      <NoteList notes={notes} />
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.addButtonText}>+ Add Note</Text>
+      </TouchableOpacity>
+      {/* Modal */}
+      <AddNoteModal
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        newNote={newNote}
+        setNewNote={setNewNote}
+        addNote={addNote}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  addButton: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    right: 20,
+    backgroundColor: '#007bff',
+    padding: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  addButtonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default NoteScreen;
